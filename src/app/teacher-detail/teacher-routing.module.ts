e1dc91b330bcec9ecc2784c92c0e8ea209d67152
Ciara <ciarampowell@gmail.com>
2017-09-23 15:02:04 -0400

    Added Comments

--- src/app/teacher-detail/teacher-routing.module.ts
+++ src/app/teacher-detail/teacher-routing.module.ts
@@ -8,3 +8,5 @@ import { CommonModule } from '@angular/common';
   declarations: []
 })
 export class TeacherRoutingModule { }
+// Would like to see Teacher page as an admin updating tool 
+// List all Teachers available for rating 
