b9de36e0b899ac702ec1f3e4a9f0e7f211f1c845
Ciara <ciarampowell@gmail.com>
2017-09-25 23:32:02 -0400

    Adding Routing to go between main page and survey page

--- src/app/teacher-detail/mock-teachers.ts
+++ src/app/teacher-detail/mock-teachers.ts
@@ -4,5 +4,6 @@ export const TEACHERS: Teacher[] = [
     { id: 11, firstName: 'Ms', lastName: 'Crabapple' },
     { id: 12, firstName: 'Mr', lastName: 'Burns' },
     { id: 13, firstName: 'Jim', lastName: 'Joe' },
+    { id: 13, firstName: 'Joe', lastName: 'Joe' }
   ];
   
