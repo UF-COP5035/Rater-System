c3a43642dcdd5d4f5824f9f94ff5d4ff0488df06
Ciara <ciarampowell@gmail.com>
2017-09-25 23:40:32 -0400

    Got rid of teacher routing module

--- src/app/teacher-detail/teachers.module.ts
+++ src/app/teacher-detail/teachers.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { TeacherDetailComponent } from './teacher-detail.component';
-import { TeacherRoutingModule } from './teacher-routing.module';
 
 // Module for Teacher Component
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
-    TeacherRoutingModule
+    FormsModule
   ],
   declarations: [
 
