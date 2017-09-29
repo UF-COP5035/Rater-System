71874093bb5323243ed8af9c07d5120876f4376d
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2017-09-29 00:46:54 -0400

    Update and fix unit tests

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,12 +6,12 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class DataService {
 
-  result:any;
+  result: any;
 
   constructor(private _http: Http) { }
 
   getUsers() {
-    return this._http.get("/api/users")
+    return this._http.get('/api/users')
       .map(result => this.result = result.json().data);
   }
 
