8d5b48a5173d5dd72eff3c8beb5e42dde21bb218
Ciara <ciarampowell@gmail.com>
2017-10-05 20:37:10 -0400

    Add routes and UI components (#62)

---
+++ src/app/teacher-dashboard/teacher-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-dashboard',
+  templateUrl: './teacher-dashboard.component.html',
+  styleUrls: ['./teacher-dashboard.component.css']
+})
+export class TeacherDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
