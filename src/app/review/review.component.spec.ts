c8d9d6933d7a87345fe399ca60c2db5905d1ba7d
Ciara <ciarampowell@gmail.com>
2017-10-14 16:04:15 -0400

    Login Page Add (#71)
    * Login_Message
    
    * Delete db.env
    
    * Adding Space for buttoon
    
    * Update login.component.css
    
    * Update .travis.yml
    
    * Edit Unit Test
    
    * Update login.component.css
    
    * Update app.component.html
    
    * Changing Grader Gator to Gator Grader
    
    * Update md to mat
    
    * Update login.component.spec.ts
    
    * Update app.component.spec.ts
    
    * Update login.component.spec.ts
    
    * Update review.service.spec.ts
    
    * Fix unit tests

--- src/app/review/review.component.spec.ts
+++ src/app/review/review.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MdInputModule, MdFormFieldModule } from '@angular/material';
+import { MatInputModule, MatFormFieldModule } from '@angular/material';
 import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,7 +19,7 @@ describe('ReviewComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [BrowserAnimationsModule, MdFormFieldModule, MdInputModule, RouterModule.forRoot(appRoutes), HttpModule],
+            imports: [BrowserAnimationsModule, MatFormFieldModule, MatInputModule, RouterModule.forRoot(appRoutes), HttpModule],
             declarations: [ReviewComponent],
             providers: [TeacherService, { provide: APP_BASE_HREF, useValue: '/' }]
         })
