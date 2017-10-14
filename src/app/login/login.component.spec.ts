c56ac4c08ef6bee1a6266f04c9f1ccff7d95b4b6
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2017-10-14 11:38:51 -0400

    Fix unit tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,17 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatInputModule, MatButtonModule, MatFormFieldModule, MatSelectModule } from '@angular/material';
+import {
+  MatCardModule, MatMenuModule,
+  MatToolbarModule, MatIconModule,
+  MatInputModule, MatButtonModule,
+  MatFormFieldModule, MatSelectModule
+} from '@angular/material';
 import { Component, OnInit, Injectable } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import { Headers, Http } from '@angular/http';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpModule, Headers, Http } from '@angular/http';
+import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+
 import { AdministratorService } from '../administrator/administrator.service';
 import { TeacherService } from '../teacher/teacher.service';
 import { StudentService } from '../student/student.service';
 import { Administrator } from '../administrator/administrator';
-import { Teacher} from '../teacher/teacher';
+import { Teacher } from '../teacher/teacher';
 import { Student } from '../student/student';
 
 import { LoginComponent } from './login.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, data: { title: 'Login' } }
+];
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -19,9 +31,11 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [MatToolbarModule, HttpModule, RouterModule.forRoot(appRoutes)],
+      declarations: [LoginComponent],
+      providers: [TeacherService, AdministratorService, StudentService, { provide: APP_BASE_HREF, useValue: '/' }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
