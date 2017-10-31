26bcb682724dfb47a7ba6ef068d9e848f2b5eebe
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2017-10-31 10:46:52 -0400

    Updated service methods for administrators and courses

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -4,13 +4,13 @@ export class Student {
     username: string;
     fullname: string;
     course_ids: string[];
-    review_id: string[];
+    review_ids: string[];
 
-    constructor(_id: number, username: string, fullname: string, course_ids: string[], review_id: string[]) {
+    constructor(_id: number, username: string, fullname: string, course_ids: string[], review_ids: string[]) {
         this._id = _id;
         this.username = username;
         this.fullname = fullname;
         this.course_ids = course_ids;
-        this.review_id = review_id;
+        this.review_ids = review_ids;
     }
 }
