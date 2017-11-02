34b9c0e214f397835b2c499bf1c65a920b138c55
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2017-11-01 23:07:02 -0400

    Update services for improved queries (#88)
    * Updated student, teachers, admin, courses routes created
    
    * Updated service methods for administrators and courses

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -3,10 +3,14 @@ export class Student {
     _id: number;
     username: string;
     fullname: string;
+    course_ids: string[];
+    review_ids: string[];
 
-    constructor(_id: number, username: string, fullname: string) {
+    constructor(_id: number, username: string, fullname: string, course_ids: string[], review_ids: string[]) {
         this._id = _id;
         this.username = username;
         this.fullname = fullname;
+        this.course_ids = course_ids;
+        this.review_ids = review_ids;
     }
 }
