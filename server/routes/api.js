0d9cf0fd51a961d74338ccc91db3b7f0a4043ba7
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2017-11-01 23:07:02 -0400

    Update services for improved queries (#88)
    * Updated student, teachers, admin, courses routes created
    
    * Updated service methods for administrators and courses

--- server/routes/api.js
+++ server/routes/api.js
@@ -125,11 +125,11 @@ router.delete('/students/:id', (req, res) => {
     });
 });
 
-/*  '/api/students/user/:username'
+/*  '/api/students/:username/user'
  *      GET: Find student by username
  */
 
-router.get('/students/user/:username', (req, res) => {
+router.get('/students/:username/user', (req, res) => {
     connection((db) => {
         db.collection('Students')
             .findOne({ username: req.params.username })
@@ -139,6 +139,100 @@ router.get('/students/user/:username', (req, res) => {
             })
             .catch((err) => {
                 sendError(err, res);
+            });
+    });
+});
+
+/*  '/api/students/:student_id/courses'
+ *      GET: Find courses by student_id
+ */
+
+router.get('/students/:student_id/courses', (req, res) => {
+    connection((student_db) => {
+        student_db.collection('Students')
+            .aggregate([
+                { $unwind: '$course_ids' },
+                { $lookup: { from: 'Courses', localField: 'course_ids', foreignField: '_id', as: 'course_info' } },
+                { $match: { _id: ObjectID(req.params.student_id) } },
+                { $lookup: { from: 'Teachers', localField: 'course_info.teacher_id', foreignField: '_id', as: 'teacher_info' } },
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    var courses = [];
+                    result.forEach(function (course) {
+                        courses.push({
+                            course_name: course.course_info[0].course_name,
+                            course_code: course.course_info[0].course_code,
+                            teacher_name: course.teacher_info[0].fullname
+                        });
+                    });
+                    response.data = courses;
+                    res.json(response);
+                }
+            });
+    });
+});
+
+/*  '/api/students/:student_id/reviews'
+ *      GET: Find reviews by student_id
+ */
+
+router.get('/students/:student_id/reviews', (req, res) => {
+    connection((student_db) => {
+        student_db.collection('Students')
+            .aggregate([
+                { $unwind: '$review_ids' },
+                { $lookup: { from: 'Reviews', localField: 'review_ids', foreignField: '_id', as: 'review_info' } },
+                { $match: { _id: ObjectID(req.params.student_id) } },
+                { $lookup: { from: 'Courses', localField: 'review_info.course_id', foreignField: '_id', as: 'course_info' } },
+                { $lookup: { from: 'Teachers', localField: 'review_info.teacher_id', foreignField: '_id', as: 'teacher_info' } },
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    console.log(result);
+                    var reviews = [];
+                    result.forEach(function (review) {
+                        reviews.push({
+                            review_content: review.review_info[0].review_content,
+                            course_name: review.course_info[0].course_name,
+                            course_code: review.course_info[0].course_code,
+                            teacher_name: review.teacher_info[0].fullname
+                        });
+                    });
+                    response.data = reviews;
+                    res.json(response);
+                }
+            });
+    });
+});
+
+/*  '/api/students/:student_id/teachers'
+ *      GET: Find teachers by student_id
+ */
+
+router.get('/students/:student_id/teachers', (req, res) => {
+    connection((student_db) => {
+        student_db.collection('Students')
+            .aggregate([
+                { $lookup: { from: 'Courses', localField: 'course_ids', foreignField: '_id', as: 'course_info' } },
+                { $match: { _id: ObjectID(req.params.student_id) } },
+                { $lookup: { from: 'Teachers', localField: 'course_info.teacher_id', foreignField: '_id', as: 'teacher_info' } }
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    var teachers = [];
+                    result[0].teacher_info.forEach(function (teacher) {
+                        teachers.push({ fullname: teacher.fullname, username: teacher.username });
+                    });
+                    response.data = teachers;
+                    res.json(response);
+                }
             });
     });
 });
@@ -236,11 +330,11 @@ router.delete('/teachers/:id', (req, res) => {
     });
 });
 
-/*  '/api/teachers/user/:username'
+/*  '/api/teachers/:username/user'
  *      GET: Find teacher by username
  */
 
-router.get('/teachers/user/:username', (req, res) => {
+router.get('/teachers/:username/user', (req, res) => {
     connection((db) => {
         db.collection('Teachers')
             .findOne({ username: req.params.username })
@@ -250,6 +344,102 @@ router.get('/teachers/user/:username', (req, res) => {
             })
             .catch((err) => {
                 sendError(err, res);
+            });
+    });
+});
+
+/*  '/api/teachers/:teacher_id/courses'
+ *      GET: Find courses by teacher_id
+ */
+
+router.get('/teachers/:teacher_id/courses', (req, res) => {
+    connection((teacher_db) => {
+        teacher_db.collection('Teachers')
+            .aggregate([
+                { $lookup: { from: 'Courses', localField: '_id', foreignField: 'teacher_id', as: 'course_info' } },
+                { $match: { _id: ObjectID(req.params.teacher_id) } },
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    var courses = [];
+                    result[0].course_info.forEach(function (course) {
+                        courses.push({
+                            course_name: course.course_name,
+                            course_code: course.course_code,
+                        });
+                    });
+                    response.data = courses;
+                    res.json(response);
+                }
+            });
+    });
+});
+
+/*  '/api/teachers/:teacher_id/reviews'
+ *      GET: Find reviews by teacher_id
+ */
+
+router.get('/teachers/:teacher_id/reviews', (req, res) => {
+    connection((teacher_db) => {
+        teacher_db.collection('Teachers')
+            .aggregate([
+                { $lookup: { from: 'Reviews', localField: '_id', foreignField: 'teacher_id', as: 'review_info' } },
+                { $match: { _id: ObjectID(req.params.teacher_id) } },
+                { $unwind: '$review_info' },
+                { $lookup: { from: 'Courses', localField: 'review_info.course_id', foreignField: '_id', as: 'course_info' } }
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    var reviews = [];
+                    result.forEach(function (review) {
+                        console.log(review);
+                        reviews.push({
+                            course_name: review.course_info[0].course_name,
+                            course_code: review.course_info[0].course_code,
+                            review_content: review.review_info.review_content,
+                        });
+                    });
+                    response.data = reviews;
+                    res.json(response);
+                }
+            });
+    });
+});
+
+/*  '/api/teachers/:teacher_id/students'
+ *      GET: Find students by teacher_id
+ */
+
+router.get('/teachers/:teacher_id/students', (req, res) => {
+    connection((teacher_db) => {
+        teacher_db.collection('Teachers')
+            .aggregate([
+                { $lookup: { from: 'Courses', localField: '_id', foreignField: 'teacher_id', as: 'course_info' } },
+                { $match: { _id: ObjectID(req.params.teacher_id) } },
+                { $unwind: '$course_info' },
+                { $lookup: { from: 'Students', localField: 'course_info._id', foreignField: 'course_ids', as: 'student_info' } }
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    var students = [];
+                    result.forEach(function (course) {
+                        course.student_info.forEach(function (student) {
+                            students.push({
+                                course_name: course.course_info.course_name,
+                                course_code: course.course_info.course_code,
+                                student_name: student.fullname,
+                            });
+                        });
+                    });
+                    response.data = students;
+                    res.json(response);
+                }
             });
     });
 });
@@ -347,11 +537,11 @@ router.delete('/administrators/:id', (req, res) => {
     });
 });
 
-/*  '/api/administrators/user/:username'
+/*  '/api/administrators/:username/user'
  *      GET: Find administrator by username
  */
 
-router.get('/administrators/user/:username', (req, res) => {
+router.get('/administrators/:username/user', (req, res) => {
     connection((db) => {
         db.collection('Administrators')
             .findOne({ username: req.params.username })
@@ -365,7 +555,137 @@ router.get('/administrators/user/:username', (req, res) => {
     });
 });
 
+/*  '/api/administrators/:administrator_id/courses'
+ *      GET: Find courses by administrator_id
+ */
 
+router.get('/administrators/:administrator_id/courses', (req, res) => {
+    connection((admin_db) => {
+        admin_db.collection('Administrators')
+            .aggregate([
+                { $lookup: { from: 'Teachers', localField: 'teacher_ids', foreignField: '_id', as: 'teacher_info' } },
+                { $match: { _id: ObjectID(req.params.administrator_id) } },
+                { $unwind: '$teacher_info' },
+                { $lookup: { from: 'Courses', localField: 'teacher_info._id', foreignField: 'teacher_id', as: 'course_info' } },
+                { $unwind: '$course_info' },
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    var courses = [];
+                    result.forEach(function (course) {
+                        courses.push({
+                            course_name: course.course_info.course_name,
+                            course_code: course.course_info.course_code,
+                            teacher_name: course.teacher_info.fullname,
+                        });
+                    });
+                    response.data = courses;
+                    res.json(response);
+                }
+            });
+    });
+});
+
+/*  '/api/administrators/:administrator_id/reviews'
+ *      GET: Find reviews by administrator_id
+ */
+
+router.get('/administrators/:administrator_id/reviews', (req, res) => {
+    connection((admin_db) => {
+        admin_db.collection('Administrators')
+            .aggregate([
+                { $lookup: { from: 'Teachers', localField: 'teacher_ids', foreignField: '_id', as: 'teacher_info' } },
+                { $match: { _id: ObjectID(req.params.administrator_id) } },
+                { $unwind: '$teacher_info' },
+                { $lookup: { from: 'Reviews', localField: 'teacher_info._id', foreignField: 'teacher_id', as: 'review_info' } },
+                { $unwind: '$review_info' },
+                { $lookup: { from: 'Courses', localField: 'review_info.course_id', foreignField: '_id', as: 'course_info' } },
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    var reviews = [];
+                    result.forEach(function (review) {
+                        reviews.push({
+                            course_name: review.course_info[0].course_name,
+                            course_code: review.course_info[0].course_code,
+                            teacher_name: review.teacher_info.fullname,
+                            review_content: review.review_info.review_content,
+                        });
+                    });
+                    response.data = reviews;
+                    res.json(response);
+                }
+            });
+    });
+});
+
+/*  '/api/administrators/:administrator_id/students'
+ *      GET: Find students by administrator_id
+ */
+
+router.get('/administrators/:administrator_id/students', (req, res) => {
+    connection((admin_db) => {
+        admin_db.collection('Administrators')
+            .aggregate([
+                { $lookup: { from: 'Teachers', localField: 'teacher_ids', foreignField: '_id', as: 'teacher_info' } },
+                { $match: { _id: ObjectID(req.params.administrator_id) } },
+                { $unwind: '$teacher_info' },
+                { $lookup: { from: 'Courses', localField: 'teacher_info._id', foreignField: 'teacher_id', as: 'course_info' } },
+                { $unwind: '$course_info' },
+                { $lookup: { from: 'Students', localField: 'course_info._id', foreignField: 'course_ids', as: 'student_info' } },
+                { $unwind: '$student_info' },
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    var students = [];
+                    result.forEach(function (student) {
+                        students.push({
+                            course_name: student.course_info.course_name,
+                            course_code: student.course_info.course_code,
+                            student_name: student.student_info.fullname,
+                        });
+                    });
+                    response.data = students;
+                    res.json(response);
+                }
+            });
+    });
+});
+
+/*  '/api/administrators/:administrator_id/teachers'
+ *      GET: Find teachers by administrator_id
+ */
+
+router.get('/administrators/:administrator_id/teachers', (req, res) => {
+    connection((admin_db) => {
+        admin_db.collection('Administrators')
+            .aggregate([
+                { $lookup: { from: 'Teachers', localField: 'teacher_ids', foreignField: '_id', as: 'teacher_info' } },
+                { $match: { _id: ObjectID(req.params.administrator_id) } },
+                { $unwind: '$teacher_info' },
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    var teachers = [];
+                    result.forEach(function (teacher) {
+                        teachers.push({
+                            teacher_name: teacher.teacher_info.fullname,
+                        });
+                    });
+                    response.data = teachers;
+                    res.json(response);
+                }
+            });
+    });
+});
 
 // COURSE API ROUTES BELOW
 
@@ -459,6 +779,34 @@ router.delete('/courses/:id', (req, res) => {
     });
 });
 
+/*  '/api/courses/:course_id/students'
+ *      GET: Find students by course_id
+ */
+
+router.get('/courses/:course_id/students', (req, res) => {
+    connection((admin_db) => {
+        admin_db.collection('Courses')
+            .aggregate([
+                { $lookup: { from: 'Students', localField: '_id', foreignField: 'course_ids', as: 'student_info' } },
+                { $match: { _id: ObjectID(req.params.course_id) } },
+                { $unwind: '$student_info' },
+            ], function (err, result) {
+                if (err) {
+                    sendError(err, res);
+                }
+                else {
+                    var students = [];
+                    result.forEach(function (student) {
+                        students.push({
+                            student_name: student.student_info.fullname,
+                        });
+                    });
+                    response.data = students;
+                    res.json(response);
+                }
+            });
+    });
+});
 
 // REVIEW API ROUTES BELOW
 
