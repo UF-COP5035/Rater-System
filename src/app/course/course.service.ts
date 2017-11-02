dab0da566b6d96ea84eba1fae0b1d5fb8f4716cb
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2017-11-01 23:07:02 -0400

    Update services for improved queries (#88)
    * Updated student, teachers, admin, courses routes created
    
    * Updated service methods for administrators and courses

--- src/app/course/course.service.ts
+++ src/app/course/course.service.ts
@@ -8,7 +8,6 @@ import { Course } from './course';
 @Injectable()
 export class CourseService {
 
-<<<<<<< HEAD
     result: any;
     private headers = new Headers({ 'Content-Type': 'application/json' });
     private coursesUrl = 'api/courses';  // URL to course api
@@ -72,59 +71,3 @@ export class CourseService {
     }
 
 } /* istanbul ignore next */
-=======
-  result: any;
-  private headers = new Headers({ 'Content-Type': 'application/json' });
-  private coursesUrl = 'api/courses';  // URL to course api
-
-  constructor(private http: Http) { }
-
-  // get("/api/courses")
-  getCourses(): Promise<Course[]> {
-    return this.http.get(this.coursesUrl)
-      .toPromise()
-      .then(response => response.json().data as Course[])
-      .catch(this.handleError);
-  }
-
-  // get("/api/courses/:id")
-  getCourse(id: string): Promise<Course> {
-    const url = `${this.coursesUrl}/${id}`;
-    return this.http.get(url)
-      .toPromise()
-      .then(response => response.json().data as Course)
-      .catch(this.handleError);
-  }
-
-  // post("/api/courses")
-  createCourse(newCourse: Course): Promise<Course> {
-    return this.http.post(this.coursesUrl, newCourse)
-      .toPromise()
-      .then(response => response.json().data as Course)
-      .catch(this.handleError);
-  }
-
-  // delete("/api/courses/:id")
-  deleteCourse(delCourseId: String): Promise<String> {
-    return this.http.delete(this.coursesUrl + '/' + delCourseId)
-      .toPromise()
-      .then(response => response.json() as String)
-      .catch(this.handleError);
-  }
-
-  // put("/api/courses/:id")
-  updateCourse(putCourse: Course): Promise<Course> {
-    const putUrl = this.coursesUrl + '/' + putCourse._id;
-    return this.http.put(putUrl, putCourse)
-      .toPromise()
-      .then(response => response.json() as Course)
-      .catch(this.handleError);
-  }
-
-  private handleError(error: any): Promise<any> {
-    console.error('Unable to retrieve courses', error);
-    return Promise.reject(error.message || error);
-  }
-
-}
->>>>>>> Add crud routes and services (#81)
