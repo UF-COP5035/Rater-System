b56553956bf1c3aeb13fddf6d464dbfc755b93b0
christopher1030123 <christopher1030123@gmail.com>
2017-11-05 01:46:00 -0500

    Adding function to calculate the reviews completed by students
    Adding function to calculate the reviews completed by students in student dashboard

--- src/app/student-dashboard/student-dashboard.component.ts
+++ src/app/student-dashboard/student-dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpModule, Http } from '@angular/http';
+import {StudentService} from '../student/student.service'
 
 @Component({
   selector: 'app-student-dashboard',
   templateUrl: './student-dashboard.component.html',
   styleUrls: ['./student-dashboard.component.css']
 })
+@Injectable()
 export class StudentDashboardComponent implements OnInit {
+  constructor(private stuser:StudentService) {}
+  Percentage(id:string) {
+    let numbercompleted=this.stuser.getReviewsByStudent(id).then.length;
+    let coursenumber=this.stuser.getCoursesByStudent(id).then.length;
+    let percentage:number=100*numbercompleted/coursenumber;
+    return percentage+"% courses has been reviewed."
+  }
 
-  constructor() { }
 
   ngOnInit() {
   }
+
 
 }
