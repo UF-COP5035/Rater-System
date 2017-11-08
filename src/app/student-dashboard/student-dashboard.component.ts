d34a894d3f6e78d9268d68e40679fec88e742adf
Ciara <ciarampowell@gmail.com>
2017-11-08 18:17:37 -0500

    Cleaned up Student Page (#89)
    * Added Angular Material and edited html
    
    * Added student, teacher, and survey components
    
    * Saved module
    
    * Added Comments
    
    * Edit Comment
    
    * Took out TeachdetailComponent
    
    * Adding Routing to go between main page and survey page
    
    * Got rid of teacher routing module
    
    * changed to get rid of teacherdetails referance in module
    
    * Added angular cdk
    
    * Update README.md (#29)
    
    * Update README.md
    
    * Update README.md
    
    * Create ISSUE_TEMPLATE.md (#32)
    
    * Create ISSUE_TEMPLATE.md
    
    * Create PULL_REQUEST_TEMPLATE.md (#31)
    
    * Create LICENSE (#30)
    
    * Update and fix unit tests (#55)
    
    * Changed api.js to fit DB (#56)
    
    * Changed api.js to fit DB
    
    Add Students, Teachers, Classes, Reviews, Administors
    
    * Update my branch
    
    * Add getID features (#59)
    
    * Add getID features
    
    When type
    Students/username
    Teachers/username
    Classes/coursename
    Reviews/coursename
    Administrators/username
    It should return a specific data
    
    * Change to ObjectID
    
    Instead of using name, we change it into using name. And by using ObjectID, it should return a distinct result
    
    * Add routes and UI components (#62)
    
    * Build fix
    
    * Fix:
    
    * Create services
    
    * Update classes and specs
    - Change classes to courses
    - Update classes to only include class properties
      - these changes have been applied to the mongo collections as well
    
    * Login_Message
    
    * Delete db.env
    
    * Adding Space for buttoon
    
    * Update login.component.css
    
    * Update .travis.yml
    
    * Edit Unit Test
    
    * Update login.component.css
    
    * Update app.component.html
    
    * Changing Grader Gator to Gator Grader
    
    * Update md to mat
    
    * Update login.component.spec.ts
    
    * Update app.component.spec.ts
    
    * Update login.component.spec.ts
    
    * Update review.service.spec.ts
    
    * Fix unit tests
    
    * Added Routing  to review from student page.
    
    * Fix Unit Test
    
    * Merge
    
    * Update api.js
    
    * Add TabsModule
    
    * Update review.component.html
    
    * Fix karma timeout

--- src/app/student-dashboard/student-dashboard.component.ts
+++ src/app/student-dashboard/student-dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule, Routes, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-student-dashboard',
